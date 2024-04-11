1a863ae78031cd14f95123695b369a473ef689c4
kochmarvin <marvin.info@gmx.at>
2024-04-11 19:11:47 +0200

    :tada: initialized the project

---
+++ projects/components/src/lib/button/button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'july-ui-button',
+  standalone: true,
+  imports: [],
+  template: ` <p>button works!</p> `,
+  styles: ``,
+})
+export class ButtonComponent {}
